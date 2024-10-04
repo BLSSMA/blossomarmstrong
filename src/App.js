4105cd924d1fa310de0ea57989b21824661fc954
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-10-04 14:12:15 -0400

    got react app initiated and 1st page skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Intro from "./Intro";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+       <Intro />
+       </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Intro.js
@@ -0,0 +1,43 @@
+
+
+export default function Intro(){
+
+    return(
+        <div className="Intro">
+        <section>
+            <h2>BLOSSOM ARMSTRONG</h2>
+            <h1>JUNIOR FRONT END DEVELOPER</h1>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+            <div>DOWNLOAD RESUME</div>
+            <div>IMG OF ME</div>
+        </section>
+        <section>
+            <div className="recentProjects">
+            <div>RECENT PROJECTS 1</div>
+            <div>
+                FIRST PROJECT TITLE
+            </div>
+            <div>12 feb 2020   |   Design, Figma, HTML</div>
+            <div>caption descriping project</div>
+         <div>RECENT PROJECTS 2</div>
+            <div>
+                FIRST PROJECT TITLE
+            </div>
+            <div>12 feb 2020   |   Design, Figma, HTML</div>
+            <div>caption descriping project</div>
+        </div>
+        </section>    
+        <section>    
+        <div className="featuredWorks">
+        <div>FEATURED WORKS</div>
+        <div className="project1">
+            <div>img</div>
+            <div>title</div>
+            <div>date and certificate</div>
+            <div>description</div>
+        </div>
+</div>
+</section>
+</div>
+    );
+}
